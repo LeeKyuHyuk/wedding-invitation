4a485faa41438a174ef8736b0035adf844b37fdc
KyuHyuk Lee <lee@kyuhyuk.kr>
2024-03-01 19:44:58 +0900

    Release: Wedding Invitation React.js Template 1.0.0

---
+++ _next/static/p8nGDEY-mi-ziJ9ijH1LI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/be6c8095f0bb1e2f.css","static/chunks/pages/index-db1de7378e68f3b3.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
